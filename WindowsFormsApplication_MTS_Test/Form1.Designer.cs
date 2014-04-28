2d26b6fe64622dc6e4b533f6c5f898369b88e01f
robojames <jlbm3d@mail.missouri.edu>
2014-04-28 15:13:15 -0600

    Second commit.

--- WindowsFormsApplication_MTS_Test/Form1.Designer.cs
+++ WindowsFormsApplication_MTS_Test/Form1.Designer.cs
@@ -30,38 +30,74 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox_MTS_CycleCounts = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(105, 191);
+            this.button1.Location = new System.Drawing.Point(149, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Test Application";
+            this.button1.Text = "Connect";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 94);
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(260, 21);
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
+            // 
+            // textBox_MTS_CycleCounts
+            // 
+            this.textBox_MTS_CycleCounts.Location = new System.Drawing.Point(12, 96);
+            this.textBox_MTS_CycleCounts.Name = "textBox_MTS_CycleCounts";
+            this.textBox_MTS_CycleCounts.Size = new System.Drawing.Size(100, 20);
+            this.textBox_MTS_CycleCounts.TabIndex = 2;
+            this.textBox_MTS_CycleCounts.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Test Polling";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Axial",
+            "Torsional"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 54);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox_MTS_CycleCounts);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ETC Echo";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +105,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox_MTS_CycleCounts;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
 
--- WindowsFormsApplication_MTS_Test/Form1.cs
+++ WindowsFormsApplication_MTS_Test/Form1.cs
@@ -12,15 +12,16 @@ using MTS793Rt;
 using MTS793Units;
 using MTS793Util;
 using System.Diagnostics;
+using Ivi.Visa.Interop;
 
 namespace WindowsFormsApplication_MTS_Test
 {
     public partial class Form1 : Form
     {
-
         enum LoadChannel { Axial, Torsional };
 
         RtSystem MTSSystem;
+        RtStation Station;
 
         public Form1()
         {
@@ -50,38 +51,74 @@ namespace WindowsFormsApplication_MTS_Test
 
                 Debug.WriteLine("Station name is: " + name);
 
-                RtStation station = MTSSystem.FindStation(name, "AppName", "", oEnumAppType.oAppSummary);
+                Station = MTSSystem.FindStation(name, "AppName", "", oEnumAppType.oAppSummary);
 
-                Debug.WriteLine(station.Channels.Count);
-
-                ObjectCollection channels = station.Channels;
-
-                RtChannel Axial = channels.Find(LoadChannel.Axial.ToString());
-
-                IRtSegGen segGen = (IRtSegGen)Axial.SegGen;
-
-                RtIntegerSig segCount = segGen.SegCountIntSignal;
-
-                MessageBox.Show((segCount.Value / 2).ToString());
-
-                     
-
-                
+                Debug.WriteLine(Station.Channels.Count);
+                                   
+          
             }
             catch (Exception err)
             {
-                MessageBox.Show(err.ToString());
+                MessageBox.Show("DANGER WILL ROBINSON--" + err.ToString());
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //try
+            //{
+            //    ResourceManager InstronRM = new ResourceManager();
 
+            //    FormattedIO488 Instron = new FormattedIO488();
+
+            //    Instron.IO = (IMessage)InstronRM.Open("GPIB0::3::INSTR", AccessMode.NO_LOCK, 20000, "");
+
+            //    MessageBox.Show("Connection established.");
+            //}
+            //catch (Exception error)
+            //{
+            //    MessageBox.Show("INSTRON ERROR: " + error.ToString());
+            //}
+
+            comboBox2.SelectedIndex = 0;
+           
+            
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             
         }
+
+
+        private void Get_Cycle_Count()
+        {
+            ObjectCollection channels = Station.Channels;
+
+            RtChannel Axial = channels.Find(comboBox2.SelectedItem.ToString());
+
+            IRtSegGen segGen = (IRtSegGen)Axial.SegGen;
+
+            RtIntegerSig segCount = segGen.SegCountIntSignal;
+
+            textBox_MTS_CycleCounts.Text = (segCount.Value / 2).ToString();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            System.Timers.Timer Refresh_Timer = new System.Timers.Timer(5000);
+
+            Refresh_Timer.Elapsed += Refresh_Timer_Elapsed;
+
+            
+        }
+
+        void Refresh_Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Get_Cycle_Count();
+        }
+
     }
 }
